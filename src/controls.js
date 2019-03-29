684a8d6c294234704ac4eb46dca19aeec85f842d
Michal Karas <mike.om4aa@gmail.com>
2019-03-29 16:37:04 +0100

    controls

--- src/controls.js
+++ src/controls.js
@@ -4,14 +4,21 @@ class TcvrControls {
 	up = '+'
 	dn = '-'
 
-	encoderAvailableFunctions = {
+	_encoderAvailableFunctions = {
 		1: [this.changeFreq],
 		2: [this.changeWpm, this.changeFilter],
 		3: [this.changeRit]
 	}
-	encoderFunction = { 1: 0, 2: 0, 3: 0 }
+	_encoderFunction = { 1: 0, 2: 0, 3: 0 }
 	
-	buttonFunctions = {
+	/**
+	 * button functions execution:
+	 * - tap: on short press
+	 * - hold: on long press
+	 * - push: immediately on press
+	 * - release: immediately on release
+	 */
+	_buttonMainFunctions = {
 		1: { tap: _ => this.switchStep() },
 		2: { tap: _ => this.switchEncoderFunction(2) },
 		3: { tap: _ => this.switchEncoderFunction(3), hold: null, push: null, release: null }, // TODO hold: zeroes current fnc (RIT/SPLIT); release: disable rit/xit/split
@@ -19,17 +26,31 @@ class TcvrControls {
 		5: { tap: _ => this.switchBand() },
 		6: { tap: _ => this.switchMode() },
 		7: { push: _ => this.setPtt(true), release: _ => this.setPtt(false) },
-		8: { tap: null, hold: null, push: null, release: null }, // TODO tap: play cwmem; hold: play cwmem nonrepeat
+		8: { tap: _ => this.buttonCwSelectFunctions(), hold: null, push: null, release: null },
+	}
+	_buttonCwSelectFunctions = {
+		1: this.buttonCwCancelFunction(1), 2: this.buttonCwCancelFunction(2), 3: this.buttonCwCancelFunction(3),
+		4: this.buttonCwFunction(4), 5: this.buttonCwFunction(5), 6: this.buttonCwFunction(6),
+		7: this.buttonCwCancelFunction(7), 8: this.buttonCwCancelFunction(8)
+	}
+
+	buttonCwFunction = btn => {
+		return { tap: _ => this.cwmem(btn), hold: _ => this.cwmem(btn, { repeat: true }) }
+	}
+	buttonCwCancelFunction = btn => {
+		return { release: _ => this.buttonMainFunctions() }
 	}
 
 	buttonTimeout = 2000
 
 	constructor(tcvr) {
 		this._tcvr = tcvr
+		this.buttonMainFunctions()
 	}
 
 	remoddleCommand(c) {
-		if (c.charCodeAt(0) <= 32) return // whitespace
+		const code = c.charCodeAt(0);
+		if (code <= 32) return // whitespace
 		// console.log('remoddle:', c)
 		if (c === '-') this.fire(new TcvrEvent(EventType.keyDah, 1))
 		else if (c === '.') this.fire(new TcvrEvent(EventType.keyDit, 1))
@@ -40,31 +61,33 @@ class TcvrControls {
 		else if (c === '[') this.rotateEncoder(2, '-') // enc2 dn
 		else if (c === '}') this.rotateEncoder(3, '+') // enc3 up
 		else if (c === '{') this.rotateEncoder(3, '-') // enc3 dn
-		else if (c === '!') this.pushButton(1) // btn1 push
-		else if (c === '~') this.pushButton(2) // btn2 push
-		else if (c === '$') this.pushButton(3) // btn3 push
-		else if (c === '^') this.pushButton(4) // btn4 push
-		else if (c === '*') this.pushButton(5) // btn5 push
-		else if (c === ':') this.pushButton(6) // btn6 push
-		else if (c === ';') this.pushButton(7) // btn7 push
-		else if (c === '`') this.pushButton(8) // btn8 push
-		else if (c === '@') this.releaseButton(1) // btn1 release
-		else if (c === '#') this.releaseButton(2) // btn2 release
-		else if (c === '%') this.releaseButton(3) // btn3 release
-		else if (c === '&') this.releaseButton(4) // btn4 release
-		else if (c === '?') this.releaseButton(5) // btn5 release
-		else if (c === '"') this.releaseButton(6) // btn6 release
-		else if (c === '|') this.releaseButton(7) // btn7 release
-		else if (c === '\'') this.releaseButton(8) // btn8 release
-		else console.error('Remoddle send unknown command:', c)
+		else if (code >= 65 && code <= 90) this.pushButton(code - 64) // A - Z
+		else if (code >= 97 && code <= 122) this.releaseButton(code - 96) // a - z
+		// else if (c === '!') this.pushButton(1) // btn1 push
+		// else if (c === '~') this.pushButton(2) // btn2 push
+		// else if (c === '$') this.pushButton(3) // btn3 push
+		// else if (c === '^') this.pushButton(4) // btn4 push
+		// else if (c === '*') this.pushButton(5) // btn5 push
+		// else if (c === ':') this.pushButton(6) // btn6 push
+		// else if (c === ';') this.pushButton(7) // btn7 push
+		// else if (c === '`') this.pushButton(8) // btn8 push
+		// else if (c === '@') this.releaseButton(1) // btn1 release
+		// else if (c === '#') this.releaseButton(2) // btn2 release
+		// else if (c === '%') this.releaseButton(3) // btn3 release
+		// else if (c === '&') this.releaseButton(4) // btn4 release
+		// else if (c === '?') this.releaseButton(5) // btn5 release
+		// else if (c === '"') this.releaseButton(6) // btn6 release
+		// else if (c === '|') this.releaseButton(7) // btn7 release
+		// else if (c === '\'') this.releaseButton(8) // btn8 release
+		else console.error('Remoddle sent unknown command:', c)
 	}
 
-	switchEncoderFunction = enc => this.encoderFunction[enc] = this._shiftIndex(this.encoderAvailableFunctions[enc], this.encoderFunction[enc])
+	switchEncoderFunction = enc => this._encoderFunction[enc] = this._shiftIndex(this._encoderAvailableFunctions[enc], this._encoderFunction[enc])
 
-	rotateEncoder = (enc, dir) => this.encoderAvailableFunctions[enc][this.encoderFunction[enc]](dir)
+	rotateEncoder = (enc, dir) => this._encoderAvailableFunctions[enc][this._encoderFunction[enc]](dir)
 
 	pushButton(btn) {
-		const fnc = this.buttonFunctions[btn]
+		const fnc = this._buttonFunctions[btn] || {}
 		fnc.push && fnc.push()
 		if (fnc.tap) fnc.time = Date.now()
 		if (fnc.hold) {
@@ -76,7 +99,7 @@ class TcvrControls {
 	}
 
 	releaseButton(btn) {
-		const fnc = this.buttonFunctions[btn]
+		const fnc = this._buttonFunctions[btn] || {}
 		fnc.release && fnc.release()
 		if (fnc.timeout != null) {
 			clearTimeout(fnc.timeout)
@@ -90,6 +113,14 @@ class TcvrControls {
 		}
 	}
 
+	buttonCwSelectFunctions() {
+		this._buttonFunctions = this._buttonCwSelectFunctions
+	}
+
+	buttonMainFunctions() {
+		this._buttonFunctions = this._buttonMainFunctions
+	}
+
 	changeFreq = dir => this._tcvr.freq = dir === '+' ? (this._tcvr.freq + this._tcvr.step) : (this._tcvr.freq - this._tcvr.step)
 	changeRit = dir => this._tcvr.freq = dir === '+' ? (this._tcvr.freq + 10) : (this._tcvr.freq - 10)
 	changeWpm = dir => this._tcvr.wpm += (dir === '+' ? 1 : -1)
@@ -100,7 +131,10 @@ class TcvrControls {
 	switchGain = _ => this._tcvr.gain = this._tcvr.gains[this._unshiftIndex(this._tcvr.gains, this._tcvr.gains.indexOf(this._tcvr.gain))]
 	switchMode = _ => this._tcvr.mode = this._shiftIndex(this._tcvr.modes, this._tcvr.mode)
 	switchFilter = _ => this._tcvr.filter = this._tcvr.filters[this._shiftIndex(this._tcvr.filters, this._tcvr.filters.indexOf(this._tcvr.filter))]
-
+	cwmem = (mem, { repeat = false }) => {
+		// TODO
+	}
+	
 	_rotateByDir(dir, list, index) {
 		return dir === '+' ? this._shiftIndex(list, index) : this._unshiftIndex(list, index)
 	}
