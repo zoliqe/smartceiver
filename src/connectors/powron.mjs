3cc64f0994707e609073b817af2c50f487cdac33
Michal Karas <zoliqe@gmail.com>
2019-09-19 06:47:31 +0200

    powron connector refactor

--- src/connectors/powron.mjs
+++ src/connectors/powron.mjs
@@ -5,8 +5,10 @@ const PowronPins = Object.freeze({pin2: 0, pin3: 1, pin4: 2, pin5: 3,
 	pinA0: 0, pinA1: 1, pinA2: 2, pinA3: 3, pinA4: 4, pinA5: 5,
 	pinA6: 6, pinA7: 7
 })
-
-// TODO refactor as usb.mjs
+const devFilters = [
+	{ 'vendorId': 0x2341, 'productId': 0x8036 },
+	{ 'vendorId': 0x2341, 'productId': 0x8037 },
+]
 
 class PowronConnector {
 	constructor({keyerPin = PowronPins.pin5, pttPin = PowronPins.pin6, powerPins = [PowronPins.pin2, PowronPins.pin4], 
@@ -14,69 +16,164 @@ class PowronConnector {
 	{
     this._encoder = new TextEncoder()
     this._decoder = new TextDecoder()
-    this._port = null
 		this._timeout = 600
 		this._keyerPin = keyerPin
 		this._pttPin = pttPin
 		this.powerPins = powerPins
 		this._serialBaudRate = serialBaudRate
-		this.connect()
+		this._interfaceNumber = 2;  // original interface number of WebUSB Arduino demo
+    this._endpointIn = 5;       // original in endpoint ID of WebUSB Arduino demo
+		this._endpointOut = 4;      // original out endpoint ID of WebUSB Arduino demo
 	}
 
-  async connect() {
-	  console.debug('powron connect request')
-    if (!serial || !navigator.usb) {
-      throw new Error('powron: WebUSB is not supported!')
+  connect(tcvr, kredence, options) {
+    if (!navigator.usb) {
+      throw new Error('POWRON: WebUSB is not supported!')
     }
-
-	this._port && this.disconnect()
-	
-	console.debug('getting serial.getPorts()')
-    const ports = await serial.getPorts()
-    console.debug(`powron getPorts(): ${JSON.stringify(ports)}`)
-    if (ports.length == 1) {
-      this._port = ports[0]
-    } else if (ports.length > 1) {
-      this._port = await serial.requestPort();
-    } else {
-      this._port = await serial.requestPort(); // TODO stop connection process, use button to connect
-    }
-
-    return new Promise((resolve, reject) => this._connectPort(resolve, reject))
+    // this.requestPort()
+    return new Promise(async (resolve, reject) => {
+			try {
+				this.device = await navigator.usb.requestDevice({ 'filters': devFilters })
+				// .then(device => {
+   	 	 	console.debug(`POWRON device: ${this.device.productName} (${this.device.manufacturerName})`)
+				await this._open()
+				// .then(port => {
+				console.log('POWRON Connected ' + this.device.productName)
+				// this._bindCommands(tcvr, port)
+				setTimeout(() => {
+					this.send(startSeq)
+					this._serialBaudRate && setTimeout(() => this.serial(this._serialBaudRate), 1000)
+				}, 3000)
+				resolve(this.device)
+			} catch (error) {
+				reject(error)
+			}
+		}, error => console.log('POWRON Connection error: ' + error))
   }
 
-  async _connectPort(resolve, reject) {
-    if (!this._port) {
-      reject('port is null')
-      return
-    }
-    console.debug(`powron device: ${this._port.device_.productName} (${this._port.device_.manufacturerName})`)
-
-    try {
-      await this._port.connect()
-      console.info('powron connected :-)')
-
-			setTimeout(() => {
-				this.send(startSeq)
-				this._serialBaudRate && setTimeout(() => this.serial(this._serialBaudRate), 1000)
-			}, 3000)
-	} catch (error) {
-      reject(error)
-      return
-    }
-    this._port.onReceive = data => console.debug('powron rcvd:', this._decoder.decode(data))
-    this._port.onReceiveError = error => this.onReceiveError(error)
-    resolve(this)
+  get connected() {
+    return true
   }
 
-  onReceiveError(error) {
-    console.error('powron error:', error)
+  _open() {
+    const readLoop = () => {
+      this.device.transferIn(this._endpointIn, 64).then(result => {
+        this.onReceive(this.decoder.decode(result.data))
+        readLoop()
+      }, error => this.onReceiveError(error))
+    }
+    return this.device.open()
+      .then(() => {
+        if (this.device.configuration === null) {
+          return this.device.selectConfiguration(1)
+        }
+			})
+			.then(() => {
+				const configurationInterfaces = this.device.configuration.interfaces
+				configurationInterfaces.forEach(element => {
+					element.alternates.forEach(elementalt => {
+						if (elementalt.interfaceClass == 0xff) {
+							this._interfaceNumber = element.interfaceNumber
+							elementalt.endpoints.forEach(elementendpoint => {
+								if (elementendpoint.direction == "out") {
+									this._endpointOut = elementendpoint.endpointNumber
+								}
+								if (elementendpoint.direction == "in") {
+									this._endpointIn = elementendpoint.endpointNumber
+								}
+							})
+						}
+					})
+				})				
+			})
+      .then(() => this.device.claimInterface(this._interfaceNumber))
+			.then(() => this.device.selectAlternateInterface(this._interfaceNumber, 0))
+      .then(() => this.device.controlTransferOut({
+        'requestType': 'class',
+        'recipient': 'interface',
+        'request': 0x22,
+        'value': 0x01,
+        'index': this._interfaceNumber
+      }))
+      .then(() => readLoop())
+  }
+
+  filter(bandWidth, centerFreq) {
+  }
+
+  checkState(kredence) {
+    return new Promise((resolve) => resolve({id: this.id})) // emulate online state
   }
 
   disconnect() {
-    this._port && this._port.disconnect()
-    this._port = null
+    return this.device && this.device.controlTransferOut({
+      'requestType': 'class',
+      'recipient': 'interface',
+      'request': 0x22,
+      'value': 0x00,
+      'index': this._interfaceNumber
+    })
+      .then(() => this.device.close())
   }
+
+  send(data) {
+		//console.debug(`POWRON <= ${data.trim()}`)
+		return this.device && this.device.transferOut(this._endpointOut, this._encoder.encode(data + '\n'))
+  }
+
+  onReceive(data) {
+    console.debug('POWRON rcvd:', this._decoder.decode(data))
+  }
+
+  onReceiveError(error) {
+    console.error('POWRON error:', error)
+  }
+
+	// async connect() {
+	//   console.debug('powron connect request')
+  //   if (!serial || !navigator.usb) {
+  //     throw new Error('powron: WebUSB is not supported!')
+  //   }
+
+	// this._port && this.disconnect()
+	
+	// console.debug('getting serial.getPorts()')
+  //   const ports = await serial.getPorts()
+  //   console.debug(`powron getPorts(): ${JSON.stringify(ports)}`)
+  //   if (ports.length == 1) {
+  //     this._port = ports[0]
+  //   } else if (ports.length > 1) {
+  //     this._port = await serial.requestPort();
+  //   } else {
+  //     this._port = await serial.requestPort(); // TODO stop connection process, use button to connect
+  //   }
+
+  //   return new Promise((resolve, reject) => this._connectPort(resolve, reject))
+  // }
+
+  // async _connectPort(resolve, reject) {
+  //   if (!this._port) {
+  //     reject('port is null')
+  //     return
+  //   }
+  //   console.debug(`powron device: ${this._port.device_.productName} (${this._port.device_.manufacturerName})`)
+
+  //   try {
+  //     await this._port.connect()
+  //     console.info('powron connected :-)')
+
+	// 		setTimeout(() => {
+	// 			this.send(startSeq)
+	// 			this._serialBaudRate && setTimeout(() => this.serial(this._serialBaudRate), 1000)
+	// 		}, 3000)
+	// } catch (error) {
+  //     reject(error)
+  //     return
+  //   }
+  //   this._port.onReceive = data => console.debug('powron rcvd:', this._decoder.decode(data))
+  //   this._port.onReceiveError = error => this.onReceiveError(error)
+  //   resolve(this)
+  // }
 
 	get timeout() {
 		return this._timeout
@@ -122,10 +219,10 @@ class PowronConnector {
 		this.send('>' + data)
 	}
 
-	send(data, callback) {
-		//console.debug(`POWRON <= ${data.trim()}`)
-    this._port && this._port.send(this._encoder.encode(data + '\n')) && console.debug(`powron sent: ${data}`)
-	}
+	// send(data, callback) {
+	// 	//console.debug(`POWRON <= ${data.trim()}`)
+  //   this._port && this._port.send(this._encoder.encode(data + '\n')) && console.debug(`powron sent: ${data}`)
+	// }
 }
 
 // device: '/dev/ttyUSB0', //'/dev/ttyS0','/dev/ttyAMA0','COM14'
