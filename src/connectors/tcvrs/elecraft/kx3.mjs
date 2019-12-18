f0f845067309661d604858893dcc89c5677d3fb2
Michal Karas <zoliqe@gmail.com>
2019-12-18 11:20:36 +0100

    remove zero gain

--- src/connectors/tcvrs/elecraft/kx3.mjs
+++ src/connectors/tcvrs/elecraft/kx3.mjs
@@ -7,7 +7,7 @@ const filters = {}
 filters[Modes.CW]  = filters[Modes.CWR] = [1800, 1500, 600, 300, 200, 100]
 filters[Modes.LSB] = filters[Modes.USB] = [2700, 2300, 2100, 1800, 1500, 1200, 1000, 800, 600]
 const gains = {}
-bands.forEach(b => gains[b] = [-10, 0, 20]) // TODO kx3 supports more preamps (10/20/30) per band - can we handle this via CAT?
+bands.forEach(b => gains[b] = [-10, 20]) // TODO kx3 supports more preamps (10/20/30) per band - can we handle this via CAT?
 
 export default {
 	model: 'kx3',
