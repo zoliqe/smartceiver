9573b4552f47751affee92899080e6e0df943fe5
Mike Karas OM4AA <mike.om4aa@gmail.com>
2019-12-21 22:35:32 +0100

    refactor ui to lit-element

--- src/connectors/tcvrs/icom.mjs
+++ src/connectors/tcvrs/icom.mjs
@@ -1,4 +1,4 @@
-import {Bands, Modes, AgcTypes} from '../../tcvr.mjs'
+import {Bands, Modes, AgcTypes} from '../../tcvr.js'
 import {delay} from '../../utils/time.mjs'
 import { resolveAgc, tcvrOptions } from './utils.mjs'
 
