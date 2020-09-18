e9f4b2836fbdabb399f9506565d1d284959b69af
Michal Karas <zoliqe@gmail.com>
2020-09-18 08:22:59 +0200

    Create smartcvr.js

---
+++ src/connectors/tcvrs/om4aa/smartcvr.js
@@ -0,0 +1,23 @@
+import { Bands, Modes, AgcTypes, TransceiverProperties } from '../../../tcvr.js'
+
+const bands = [160, 80, 40, 30, 20, 17, 15, 12, 10]
+const filters = {}
+filters[Modes.CW]  = [4000, 3000, 2200, 1200, 1000, 500]
+filters[Modes.CWR] = filters[Modes.CW]
+filters[Modes.LSB] = filters[Modes.CW]
+filters[Modes.USB] = filters[Modes.CW]
+const gains = {}
+bands.forEach(b => {gains[b] = [-10]})
+
+export default {
+	model: 'smartcvr',
+	baudrate: 115200,
+	props: new TransceiverProperties({
+		bands,
+		modes: [Modes.CW, Modes.CWR, Modes.LSB, Modes.USB],
+//		agcTypes: [AgcTypes.FAST, AgcTypes.SLOW, AgcTypes.AUTO],
+		bandGains: gains,
+		modeFilters: filters
+	}),
+	defaults: {band: 20, mode: Modes.CW/*, agc: AgcTypes.FAST*/}
+}
