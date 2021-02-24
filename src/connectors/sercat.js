8e02c55929ca846d23e00b100edc42f02031f447
Michal Karas <zoliqe@gmail.com>
2021-02-24 20:52:21 +0100

    use serial interface

--- src/connectors/sercat.js
+++ src/connectors/sercat.js
@@ -1,34 +1,19 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-expressions */
 import {SignalsBinder} from '../utils/signals.js'
-
-// const devFilters = [
-// 	{ 'vendorId': 0x2341, 'productId': 0x8036 },
-// 	{ 'vendorId': 0x2341, 'productId': 0x8037 },
-// ]
-const encoder = new TextEncoder()
-const decoder = new TextDecoder()
+import {SerialInterface} from '../interfaces/serial.js'
 
 class SercatConnector {
-	#device
 
-	#reader
-
-	#adapter
+	#iface
 
 	#signals
 
-	#devopts = {
-		baudRate: 4800,
-//     dataBits: 8,
-//     parity: 'none',
-//     stopBits: 1,
-//     flowControl: 'none'
-	}
-
 	constructor(tcvrAdapter) {
 		this.#adapter = tcvrAdapter
-		this.#devopts.baudRate = this.#adapter.baudrate
+		this.#iface = new SerialInterface(this.#adapter.baudrate)
+    this.#iface.receive = this.onReceive
+    this.#iface.receiveError = this.onReceiveError
 
 		this._initSignals()
 	}
@@ -38,53 +23,28 @@ class SercatConnector {
 	}
 
 	async connect() {
-		if (!navigator.serial) {
-			window.alert('Serial not supported. Cannot connect to transceiver.')
-			throw new Error('SERCAT: WebSerial is not supported!')
-		}
 		try {
-			this.#device = await navigator.serial.requestPort({})
-			const info = this.#device.getInfo()
-			console.debug(`SERCAT device: num=${info.serialNumber} ${info.product} (${info.manufacturer})`)
-			await this.#device.open(this.#devopts)
-			console.info('SERCAT Connected', info.product)
-			await this._on()
-			this._readLoop()
+      await this.#iface.connect()
+      await this._on()
 		} catch (error) {
+      if (error === 'unsupported') {
+        window.alert('Serial not supported by browser. Cannot connect to transceiver.')
+        throw new Error('SERCAT: API is not supported!')
+      }
 			console.error('SERCAT Connection error:', error)
 			throw error
 		}
 		return this
 	}
 	
-	async _readLoop() {
-		while (this.#device.readable) {
-			try {
-				this.#reader = this.#device.readable.getReader()
-				while (true) {
-					const {value, done} = await this.#reader.read()
-					this.onReceive(decoder.decode(value))
-					if (done) break
-				}
-				this.#reader = null
-			} catch (e) {
-				this.onReceiveError(e)
-			}
-		}
-	}
-
 	async disconnect() {
-		if (!this.#device) return
-
+		if (!this.connected) return
 		await this._off()
-		if (this.#reader)
-			this.#reader.cancel()
-		await this.#device.close()
-		this.#device = null
+    await this.#iface.disconnect()
 	}
 
 	async _on() {
-		this.#adapter.init && (await this.#adapter.init(async (data) => this._send(data)))
+		this.#adapter.init && (await this.#adapter.init(async (data) => this.#iface.send(data)))
 	}
 
 	async _keepAlive() {
@@ -96,7 +56,7 @@ class SercatConnector {
 	}
 
 	get connected() {
-		return this.#device != null
+		return this.#iface.connected
 	}
 
 	async checkState() {
@@ -109,19 +69,6 @@ class SercatConnector {
 
 	get tcvrDefaults() {
 		return this.#adapter.defaults
-	}
-
-	async _send(data) {
-		console.debug(`SERCAT <= ${data}`)
-		if (this.connected && this.#device.writable) {
-			const writer = this.#device.writable.getWriter()
-			const bytes = typeof data === 'string' ? encoder.encode(data) : data
-			writer.write(bytes)
-			writer.releaseLock()
-			return true
-		}
-		console.error(`SERCAT: data not sent ${data}`)
-		return false
 	}
 
 	onReceive(data) {
