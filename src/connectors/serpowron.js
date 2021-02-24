f6fd3a1f3810bb1f7e2a4c4ed61c437994b19900
Michal Karas <zoliqe@gmail.com>
2021-02-24 21:06:16 +0100

    use serial interface

--- src/connectors/serpowron.js
+++ src/connectors/serpowron.js
@@ -1,34 +1,19 @@
 /* eslint-disable class-methods-use-this */
 import { delay } from '../utils/time.js'
 import { Powron, Pins } from './extensions/powron.js'
-
-// not supported by navigator
-const devFilters = [
-	{ usbVendorId: 0x2341, usbProductId: 0x8036 },
-	{ usbVendorId: 0x2341, usbProductId: 0x8037 },
-	{ usbVendorId: 0x1A86, usbProductId: 0x7523 },
-]
-const encoder = new TextEncoder()
-const decoder = new TextDecoder()
+import { SerialInterface } from '../interfaces/serial.js'
 
 class PowronConnector {
 
-	#device
+	#iface
 	
 	#powron
 	
-	#reader
-
-	#devopts = {
-		baudRate: 4800,
-//     dataBits: 8,
-//     stopBits: 1,
-//     parity: 'none',
-//     flowControl: 'none'
-	}
-
 	constructor(tcvrAdapter, {options, keyerConfig}) {
-		this.#powron = new Powron(tcvrAdapter, async (cmd) => this._send(cmd), {options, keyerConfig})
+		this.#powron = new Powron(tcvrAdapter, async (cmd) => this.#iface.send(cmd), {options, keyerConfig})
+		this.#iface = new SerialInterface(this.#adapter.baudrate)
+    this.#iface.receive = this.onReceive
+    this.#iface.receiveError = this.onReceiveError
 	}
 
 	get id() {
@@ -36,66 +21,42 @@ class PowronConnector {
 	}
 
 	async connect() {
-		if (!navigator.serial) {
-			window.alert('Serial not supported. Cannot connect to transceiver.')
-			throw new Error('POWRON: WebSerial is not supported!')
-		}
 		try {
-			this.#device = await navigator.serial.requestPort({filters: devFilters})
-			const info = this.#device.getInfo()
-			console.debug(`POWRON device: num=${info.serialNumber} ${info.product} (${info.manufacturer})`)
-			await this.#device.open(this.#devopts)
-			console.info('POWRON Connected')
-			window.powronDevice = this.#device
-			this._readLoop()
+      await this.#iface.connect()
 			await this.#powron.init()
 		} catch (error) {
-			console.error('POWRON Connection error:', error)
+      if (error === 'unsupported') {
+        window.alert('Serial not supported by browser. Cannot connect to transceiver.')
+        throw new Error('SERCAT: API is not supported!')
+      }
+			console.error('SERCAT Connection error:', error)
 			throw error
 		}
 		return this
 	}
 	
-	async _readLoop() {
-		while (this.#device.readable) {
-			this.#reader = this.#device.readable.getReader()
-// 			console.debug('reader get')
-			while (true) {
-				let value, done
-				try {
-					// eslint-disable-next-line no-await-in-loop
-					({value, done} = await this.#reader.read())
-				} catch (e) {
-					this.#powron.onReceiveError(e)
-					break;
-				}
-// 				console.debug('POWRON RAW:', value)
-				value && this.#powron.onReceive(decoder.decode(value))
-				if (done) break
-			}
-// 			console.debug('reader releaseLock')
-			this.#reader.releaseLock()
-			this.#reader = null
-		}
-	}
-
 	async disconnect() {
-		if (!this.#device) return
+		if (!this.connected) return
 
 		await delay(1000) // for poweroff signals 
-		if (this.#reader)
-			await this.#reader.cancel()
-		await this.#device.close()
-		this.#device = null
+    await this.#iface.disconnect()
 	}
 
 	get connected() {
-		return this.#device != null
+		return this.#iface.connected
 	}
 
 	async checkState() {
 		// TODO maybe check present device by navigator.usb.getDevices()?
 		return {id: this.id} // this.connected ? {id: this.id} : null
+	}
+	
+	onReceive(data) {
+		console.debug('SERPWRON rcvd:', data)
+	}
+
+	onReceiveError(error) {
+		console.error('SERPWRON error:', error)
 	}
 	
 	get tcvrProps() {
@@ -104,20 +65,6 @@ class PowronConnector {
 
 	get tcvrDefaults() {
 		return this.#powron.tcvrDefaults
-	}
-
-	async _send(data) {
-		console.debug(`POWRON <= ${data} `)
-		if (this.connected && this.#device.writable) {
-			const writer = this.#device.writable.getWriter() // get WritableStreamDefaultWriter
-			await writer.ready // wait while stream locked
-			const bytes = typeof data === 'string' ? encoder.encode(`${data}\n`) : data
-			await writer.write(bytes)
-			writer.releaseLock()
-			return true
-		}
-		console.error(`POWRON: data not sent ${data}`)
-		return false
 	}
 
 	get signals() {
