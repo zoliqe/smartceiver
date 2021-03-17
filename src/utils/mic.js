c50c4d2755474b708e0ebb3f255a0e158cacc0e2
Michal Karas <zoliqe@gmail.com>
2021-03-17 21:24:26 +0100

    enumerateDevices - select audio source

--- src/utils/mic.js
+++ src/utils/mic.js
@@ -3,36 +3,67 @@
 // import {SignalType} from './signals.js'
 
 class Microphone {
-	constructor() {
-		this.userMediaConstraints = {
-			video: false,
-			audio: {
-				sampleRate: 8000,
-				// sampleSize: 16,
-				channelCount: 1,
-				volume: 1.0,
-				autoGainControl: false,
-				echoCancellation: false,
-				noiseSuppression: false
-			}
+	
+	#userMediaConstraints = {
+		video: false,
+		audio: {
+			sampleRate: 8000,
+			// sampleSize: 16,
+			channelCount: 1,
+			volume: 1.0,
+			autoGainControl: false,
+			echoCancellation: false,
+			noiseSuppression: false
 		}
+	}
+	
+	#stream
+	
+	#track
+
+	constructor() {
 		// this.tcvr = tcvr
 	}
 
-	request() {
-		console.debug('Requesting user microphone with constraints', this.userMediaConstraints)
-		return navigator.mediaDevices.getUserMedia(this.userMediaConstraints)
-			.then(stream => {
-				this._stream = stream
-				this._track = stream.getAudioTracks()[0]
-				console.debug('Adding microphone', stream, this._track)
-				this._track && console.info('Microphone constraints:', this._track.getSettings())
-				// this.mute()
-				// this.tcvr.bind(SignalType.ptt, 'mic', 
-				// 	event => event.value ? this.unmute() : this.mute())
-				return this
-			})
-			.catch(error => window.alert(`Request to access your microphone was unsucessfull.\nSSB transmit will not be available.\nError: ${error.name}`))
+	async request(
+// 		deviceLabels = ['Wired headset', 'USB Audio Device: USB Audio:2,0: Mic', 'USB Audio Device Analog Stereo', 'Audio Adapter (Planet UP-100, Genius G-Talk) Mono', 'Generic USB Audio Device: USB Audio:3,0: Mic']) {
+		deviceLabels = ['headset', 'Headset', 'USB Audio Device', 'Audio Adapter']
+	) {
+		this.#userMediaConstraints.audio.audioId = await this._findDeviceIdByLabel(deviceLabels)
+		console.debug('Requesting user microphone with constraints', this.#userMediaConstraints)
+		
+		try {
+			this.#stream = await navigator.mediaDevices.getUserMedia(this.#userMediaConstraints)
+		} catch (error) {
+			window.alert(`Request to access your microphone was unsucessfull.\nSSB transmit will not be available.\nError: ${error.name}`))
+			throw error
+		}
+		
+		this.#track = this.#stream.getAudioTracks()[0]
+		console.debug('Adding microphone', this.#stream, this.#track)
+		this.#track && console.info('Microphone constraints:', this.#track.getSettings())
+		// this.mute()
+		// this.tcvr.bind(SignalType.ptt, 'mic', 
+		// 	event => event.value ? this.unmute() : this.mute())
+		return this.#stream
+	}
+
+	async _findDeviceIdByLabel(labelsFilter) {
+		try {
+			const allDevices = await navigator.mediaDevices.enumerateDevices()
+			console.debug('AudioProcessor: Found these audioinput devices:', allDevices)
+			const devices = allDevices
+				.filter(device => device.kind === 'audioinput')
+				.filter(device => labelsFilter.some(labelFilter => device.label.includes(labelFilter)))
+			console.info('AudioProcessor: Selected these audioinput devices (using first, if one found):', devices)
+			if (devices.length === 1 && navigator.userAgent.includes('Android')) {
+				alert('Selected audio input: ' + devices[0].label)
+			}
+			return devices.length === 1 ? devices[0].deviceId : null // only when exactly one device found, otherwise user must select default device in OS
+		} catch (e) {
+			console.error('Error enumerating mediaDevices:', e)
+			return null
+		}
 	}
 
 	close() {
