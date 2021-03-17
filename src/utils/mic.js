24643287daaf8aaa789ccc06b6a6c713285bffc0
Michal Karas <zoliqe@gmail.com>
2021-03-17 22:11:58 +0100

    Wired headset

--- src/utils/mic.js
+++ src/utils/mic.js
@@ -27,7 +27,7 @@ class Microphone {
 
 	async request(
 // 		deviceLabels = ['Wired headset', 'USB Audio Device: USB Audio:2,0: Mic', 'USB Audio Device Analog Stereo', 'Audio Adapter (Planet UP-100, Genius G-Talk) Mono', 'Generic USB Audio Device: USB Audio:3,0: Mic']) {
-		deviceLabels = ['headset', 'Headset', 'USB Audio Device', 'Audio Adapter']
+		deviceLabels = ['Wired headset', 'USB Audio Device', 'Audio Adapter']
 	) {
 		this.#userMediaConstraints.audio.deviceId = await this._findDeviceIdByLabel(deviceLabels)
 		console.debug('Microphone: Requesting user microphone with constraints', this.#userMediaConstraints)
