8c109508f6c7271205db9dc89386c75c2948a630
Michal Karas <zoliqe@gmail.com>
2021-12-24 14:25:26 +0100

    bobik: bands

--- src/connectors/tcvrs/om4aa/bobik.js
+++ src/connectors/tcvrs/om4aa/bobik.js
@@ -1,11 +1,11 @@
 import { Bands, Modes, AgcTypes, TransceiverProperties } from '../../../tcvr.js'
 
-const bands = [160, 80, 40, 30, 20, 17, 15, 12, 10]
+const bands = [80] //[160, 80, 40, 30, 20, 17, 15, 12, 10]
 const filters = {}
-filters[Modes.CW]  = [4000, 3000, 2200, 1200, 1000, 500]
+filters[Modes.CW]  = [1200, 500]
 filters[Modes.CWR] = filters[Modes.CW]
-filters[Modes.LSB] = filters[Modes.CW]
-filters[Modes.USB] = filters[Modes.CW]
+filters[Modes.LSB] = [4000, 3000, 2200, 1000]
+filters[Modes.USB] = filters[Modes.LSB]
 const gains = {}
 bands.forEach(b => {gains[b] = [-10]})
 
@@ -19,5 +19,5 @@ export default {
 		bandGains: gains,
 		modeFilters: filters
 	}),
-	defaults: {band: 20, mode: Modes.CW, agc: AgcTypes.AUTO}
+	defaults: {band: 80, mode: Modes.CW, agc: AgcTypes.AUTO}
 }
