6ac0d63eb11797a6fa06ee9fbe69c6519393d006
Michal Karas <zoliqe@gmail.com>
2022-07-06 01:18:39 +0200

    fix

--- src/controllers/ui/SmartceiverApp.js
+++ src/controllers/ui/SmartceiverApp.js
@@ -543,7 +543,7 @@ export class SmartceiverApp extends LitElement {
 		// instant connect
 		setTimeout(() => {
 			if ((this.connectors.pwr && this.connectors.pwr.connected) || (this.connectors.cat && this.connectors.cat.connected)) {
-				await this.connectPower()
+				this.connectPower()
 			}
 		}, 500)
 	}
